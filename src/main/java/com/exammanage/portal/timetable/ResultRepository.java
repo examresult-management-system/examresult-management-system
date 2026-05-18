e8736c375528cc31dd6b2d482d4d366ad8b98261
IT25103311 <it25103311@my.sliit.lk>
2026-05-18 23:56:03 +0530

    IT25103311
    IT25103311

---
+++ src/main/java/com/exammanage/portal/timetable/ResultRepository.java
@@ -0,0 +1,9 @@
+package com.exammanage.portal.result;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ResultRepository extends JpaRepository<Result, Long> {
+
+}
