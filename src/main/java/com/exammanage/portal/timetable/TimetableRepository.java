b4b2f6a5cf49625c745da1842fe799e6457bb02f
IT25103311 <it25103311@my.sliit.lk>
2026-05-18 18:20:35 +0530

    IT25103311
    IT25103311

---
+++ src/main/java/com/exammanage/portal/timetable/TimetableRepository.java
@@ -0,0 +1,9 @@
+package com.exammanage.portal.timetable;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface TimetableRepository extends JpaRepository<Timetable, Long> {
+
+ }
