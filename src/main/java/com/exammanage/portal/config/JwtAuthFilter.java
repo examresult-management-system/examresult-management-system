7358fe72a87bbb2765e1db1366f8523e39897c10
Nawarathne N.M.L.R.B <it25101873@my.sliit.lk>
2026-05-19 09:20:17 +0530

    IT25101873

---
+++ src/main/java/com/exammanage/portal/config/JwtAuthFilter.java
@@ -0,0 +1,27 @@
+package com.exammanage.portal.config;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+@Component
+public class JwtAuthFilter extends OncePerRequestFilter {
+
+    @Override
+    protected void doFilterInternal(
+            HttpServletRequest request,
+            HttpServletResponse response,
+            FilterChain filterChain
+    ) throws ServletException, IOException {
+
+        // No real JWT validation for now.
+        // Frontend can still send dummy token without backend error.
+        filterChain.doFilter(request, response);
+    }
+}
